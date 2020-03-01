944053a395a33c50555961c165c805d9a595ea7d
Matt Greenlaw <walneergwehttam@googlemail.com>
2020-03-01 07:52:50 -0800

    Add LocalDb backend

---
+++ Models/Calendar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace APSReactTutorial.Models
+{
+    public class Calendar
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+
+    public class CalendarDBContext : DbContext
+    {
+        public DbSet<Calendar> Calendars { get; set; }
+    }
+}
